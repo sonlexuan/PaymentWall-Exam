ca2b2fcd2710675736606d758562a004a412ed47
Son Le <lexuanson94@gmail.com>
2020-05-09 06:13:57 +0700

    Config Deploy

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/paymentwall-test'));
+app.use(express.static('./dist/payment-wall-exam'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/paymentwall-test/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/payment-wall-exam/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
